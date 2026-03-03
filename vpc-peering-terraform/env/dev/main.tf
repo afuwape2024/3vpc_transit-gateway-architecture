1905712aa0d1b1e72d3d57a7418e760bbbbdfbff
afuwape2024 <afuwape2024@gmail.com>
2026-03-03 06:12:08 +0000

    first

--- vpc-peering-terraform/env/dev/main.tf
+++ vpc-peering-terraform/env/dev/main.tf
@@ -30,3 +30,20 @@ module "columbus_vpc" {
   columbus_db_subnet_cidr_block  = var.columbus_db_subnet_cidr_block
   availability_zones             = var.availability_zones
 }
+
+module "internet_gateway" {
+  source = "../../modules/internet_gateway"
+  chicago_vpc = module.chicago_vpc.chicago_vpc
+  detroit_vpc = module.detriot_vpc.detroit_vpc
+  columbus_vpc = module.columbus_vpc.columbus_vpc
+}
+
+
+module "route_table" {
+  source = "../../modules/route_table"
+  detroit_vpc = module.detriot_vpc.detroit_vpc
+  detroit_vpc_public_subnet = module.detriot_vpc.detroit_vpc_public_subnet
+  detroit_vpc_private_subnet = module.detriot_vpc.detroit_vpc_private_subnet
+  detroit_vpc_public2_subnet = module.detriot_vpc.detroit_vpc_public2_subnet
+  detroit_vpc_private2_subnet = module.detriot_vpc.detroit_vpc_private2_subnet    
+}
